1776143f79f796faab7564dd62a61ac11a38298f
Kanachai <kanachai@teibto.com>
2025-04-29 21:07:38 +0700

    Move Auth to Library

--- src/pages/api/job-experience.ts
+++ src/pages/api/job-experience.ts
@@ -1,48 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { format } from 'date-fns';
 import JobExperience from '@/models/JobExperience';
-//import jwt from 'jsonwebtoken';
 import connectDB from '@/lib/connectDB';
 import checkAuth from "@/lib/checkAuth";
 
 
 
-// const JWT_USER = process.env.JWT_USER;
-// const JWT_ROLE = process.env.JWT_ROLE;
-//
-//
-// const checkAuth = (req: NextApiRequest): boolean => {
-//
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader || !authHeader.startsWith('Bearer')) return false;
-//     const token = authHeader.split(' ')[1];
-//
-//     if (!token) {
-//         return false;
-//     }
-//
-//     try {
-//         // Verify JWT Token
-//         if (!process.env.JWT_SECRET) {
-//             throw new Error('JWT_SECRET is not defined in environment variables.');
-//         }
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//
-//
-//         if (typeof decoded !== 'string' ) {
-//
-//
-//             const {userId, role} = decoded;
-//
-//             return userId===JWT_USER && role===JWT_ROLE;
-//         }
-//         else return false;
-//
-//     }
-//     catch{
-//         return false;
-//     }
-// };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
--- src/pages/api/skill.ts
+++ src/pages/api/skill.ts
@@ -5,44 +5,6 @@ import connectDB from '@/lib/connectDB';
 import checkAuth from "@/lib/checkAuth";
 
 
-// const JWT_USER = process.env.JWT_USER;
-// const JWT_ROLE = process.env.JWT_ROLE;
-//
-//
-// const checkAuth = (req: NextApiRequest): boolean => {
-//
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader || !authHeader.startsWith('Bearer')) return false;
-//     const token = authHeader.split(' ')[1];
-//
-//     if (!token) {
-//         return false;
-//     }
-//
-//     try {
-//         // Verify JWT Token
-//         if (!process.env.JWT_SECRET) {
-//             throw new Error('JWT_SECRET is not defined in environment variables.');
-//         }
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//
-//         // ดึงข้อมูลจาก decoded payload (userId, role)
-//         if (typeof decoded !== 'string' ) {
-//
-//
-//             const {userId, role} = decoded;
-//
-//             return userId===JWT_USER && role===JWT_ROLE;
-//         }
-//         else return false;
-//
-//
-//     }
-//     catch{
-//         return false;
-//     }
-// };
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (!checkAuth(req)) {
